54cfa495ab6d3963b0aa9f0e700835d42b13204c
nomofica <bltruscott1@gmail.com>
2016-06-02 00:40:00 -0600

    Add files via upload

---
+++ nbfc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Auto-start script for NBFC (Notebook Fan Controller) written by Brian Truscott
+
+sudo sh /opt/nbfc/Linux/bin/ReleaseLinux/start-nbfcservice.sh
+mono /opt/nbfc/Linux/bin/ReleaseLinux/nbfc.exe config --apply default
+
+# Uncomment below only to troubleshoot when manually running script
+# mono /opt/nbfc/Linux/bin/ReleaseLinux/nbfc.exe status -a
