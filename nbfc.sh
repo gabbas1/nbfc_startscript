f61eaaa00ae5c5075a2de1f14867b305ad796ee3
gabbas1 <51380897+gabbas1@users.noreply.github.com>
2020-08-02 12:28:29 +0200

    Update nbfc.sh
    1) folder structure of nbfc has changed from /opt/nbfc/Linux/bin/ReleaseLinux /.. to /opt/nbfc/Linux/bin/Release/
    2) the start-nbfcservice.sh is now "nbfcservice.sh start"

--- nbfc.sh
+++ nbfc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Auto-start script for NBFC (Notebook Fan Controller) written by Brian Truscott
 
-sudo sh /opt/nbfc/Linux/bin/ReleaseLinux/start-nbfcservice.sh
-mono /opt/nbfc/Linux/bin/ReleaseLinux/nbfc.exe config --apply default
+sudo sh /opt/nbfc/Linux/bin/Release/nbfcservice.sh start
+mono /opt/nbfc/Linux/bin/Release/nbfc.exe config --apply default
 
 # Check status of NBFC when manually running script
-mono /opt/nbfc/Linux/bin/ReleaseLinux/nbfc.exe status -a
+mono /opt/nbfc/Linux/bin/Release/nbfc.exe status -a
